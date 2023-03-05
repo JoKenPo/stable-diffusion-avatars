b020882efc4cedf0f8c364056aae1e457fc80f58
Eduardo Almeida <duduf2005@hotmail.com>
2023-03-05 16:58:25 -0300

    feat: winston log

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,62 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { Server } from 'http';
 import router from './routes';
+import winston from 'winston';
+import expressWinston from 'express-winston';
 
+const loggerOptions: expressWinston.LoggerOptions = {
+	transports: [new winston.transports.Console()],
+	format: winston.format.combine(
+		winston.format.json(),
+		winston.format.prettyPrint(),
+		winston.format.colorize({ all: true }),
+	),
+  meta: false,
+};
+
+const notFoundHandler = (
+	request: express.Request,
+	response: express.Response,
+	next: express.NextFunction,
+) => {
+	const message = 'Resource not found';
+	response.status(404).json({ status: 404, message });
+};
+
+const errorHandler = (
+	error: HttpException,
+	request: express.Request,
+	response: express.Response,
+	next: express.NextFunction,
+) => {
+	const status = error.statusCode || error.status || 400;
+	response.status(status).json({ status, ...error });
+};
+
+class HttpException extends Error {
+	statusCode?: number;
+	status?: number;
+	message: string;
+	error: string | null;
+
+	constructor(statusCode: number, message: string, error?: string) {
+		super(message);
+
+		this.statusCode = statusCode;
+		this.message = message;
+		this.error = error || null;
+	}
+}
 export class SetupApplication {
   private server?: Server;
 
-  constructor(private port = 3000, public app = express()) { }
+  constructor(private port = 3030, public app = express()) { }
 
   public init(): void {
     this.setupExpress();
+    this.setupLogger();
     this.setupRoutes();
+
   }
 
   private setupRoutes(): void {
@@ -24,9 +71,14 @@ export class SetupApplication {
     this.app.use(bodyParser.urlencoded({ extended: true }));
   }
 
+  private setupLogger(): void {
+		this.app.use(expressWinston.logger(loggerOptions));
+  }
+
   public start(): void {
     this.server = this.app.listen(this.port, () => {
-      console.log(`Server running on port ${this.port}`);
+  	  console.log('Server environment: ', process.env.NODE_ENV?.toLowerCase());
+      console.log(`Server running at http://localhost:${this.port} 🚀`);
     });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 import { SetupApplication } from './app';
 
+
 class Server {
     static start(): void {
-        const application = new SetupApplication(3000);
-        application.init();
-        application.start();
+        const app = new SetupApplication(parseInt(process.env.PORT as string, 10) | 3030);
+        app.init();
+
+        app.start();
     }
 }
 
